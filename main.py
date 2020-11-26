46d7343bff1349feda53e093713d26d0c847ee0a
Amir Khalesi <amir.pured@gmail.com>
2020-11-26 21:28:01 +0330

    added /idea and edited main, containing the backbone

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+import time
+import os
+
+
+def time_check (target_time):
+    pass
+
+def play_alarm ():
+    pass
+
+def main():
+    pass
+
+
+if '__name__' == '__main__':
+    main()
