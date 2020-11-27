e9422187a905013842b4c6098236d3289a0b1123
Amir Khalesi <amir.pured@gmail.com>
2020-11-27 23:07:57 +0330

    added the functional main.py

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-import sys
 import time
-import os
+import datetime
+from subprocess import call
+from utils.input_time import read_input_time
+from utils.play_alarm import play_alarm
+from utils.time_check import time_check
+from utils.show_live_time import show_live_time
 
 
-def time_check (target_time):
-    pass
-
-def play_alarm ():
-    pass
 
 def main():
-    pass
+    target_time = 1
+    call(['clear'])  # uses a subprocess to show you the time
+    print('welcome to chomagh!')
 
+    today = show_live_time()
+    time.sleep(1)
 
-if '__name__' == '__main__':
+    target_time = read_input_time(today)
+
+    time_check(target_time)
+
+    play_alarm()
+
+if __name__ == '__main__':
     main()
